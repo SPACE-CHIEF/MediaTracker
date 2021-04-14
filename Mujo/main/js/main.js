3b3a69b9ab10aebe506af27d1d16024d5f1368c4
= <=>
2021-04-13 22:15:51 -0400

    modal is broken (reeeee)

--- Mujo/main/js/main.js
+++ Mujo/main/js/main.js
@@ -1,11 +1,11 @@
-(function($) {
+(function ($) {
 
 	"use strict";
 
-	var fullHeight = function() {
+	var fullHeight = function () {
 
 		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
+		$(window).resize(function () {
 			$('.js-fullheight').css('height', $(window).height());
 		});
 
@@ -13,20 +13,18 @@
 	fullHeight();
 
 	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
+		$('#sidebar').toggleClass('active');
+	});
 
 })(jQuery);
 
-chrome.runtime.sendMessage({"command": "getMovies"}, (response) => {
+chrome.runtime.sendMessage({ "command": "getMovies" }, (response) => {
 
 	document.querySelector("#movieRow").innerHTML = "";
-    for (const key in response.data) {
-        
+	for (const key in response.data) {
 		var posterURL = ""
-
 		const baseImagePath = "https://image.tmdb.org/t/p/original/"
-		
+
 		fetch(`https://api.themoviedb.org/3/search/movie?api_key=c9e5f6b44a8037f35894aef02579205a&language=en-US&query=${key}&page=1`)
 			.then(response => {
 				return response.json()
@@ -34,71 +32,129 @@ chrome.runtime.sendMessage({"command": "getMovies"}, (response) => {
 			.then(data => {
 				var movieID = data.results[0].id
 				posterURL = baseImagePath + data.results[0].poster_path
-				var htmlToAdd = 
-				`<div class="col-md-6 col-lg-4 col-xl-3 pb-4">
-					<div class="card h-100">
-						<img src="${posterURL}" class="card-img-top" alt="...">
-						<div class="card-body">
+				fetch(`https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=c9e5f6b44a8037f35894aef02579205a&language=en-US`)
+				.then(response => {
+					return response.json();
+				})
+				.then(credits => {
+					let addModal = ``
+					let crew = credits["crew"];
+					for(const member of crew){
+						console.log(member.name)
+						if(member.known_for_department == "Directing"){
+							addModal += `<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
+							<div class="modal-dialog modal-dialog-centered">
+							  <div class="modal-content">
+									<div class="modal-header">
+									  <h5 class="modal-title" id="myModalLabel">Title</h5>
+									  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+									  </button>
+									</div>
+									<div class="modal-body">
+									  <p><a href="${response.data[key]}" target="_blank">Keep watching</a></p>
+									  <p>Director: ${member.name} </p>
+									</div>
+									<div class="modal-footer">
+									  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									  <button type="button" class="btn btn-primary">Save changes</button>
+									</div>
+							  </div>
+							</div>`
+							break;
+						}
+					}
+					document.querySelector("#movieRow").innerHTML += addModal;
+				})
+				var htmlToAdd =
+				`
+				<div class="col-md-6 col-lg-4 col-xl-3 pb-4">
+					<div class="card h-100">					
+						<img src="${posterURL}" class="card-img-top" alt="..."/>
+						<div class="card-body">	
 							<h5 class="card-title">${key}</h5>
 							<div class="card-footer">
 								<small class="text-muted">Rating: 4/5</small>
 							</div>
+							<a class="stretched-link" role="button" data-toggle="modal" data-target="#myModal"></a>
 						</div>
 					</div>
-				</div>`
+				</div>
+				`
 				document.querySelector("#movieRow").innerHTML += htmlToAdd;
 			})
 			.catch(error => {
 				console.log(error);
 			})
-    }
-
+	}
 });
 
-chrome.runtime.sendMessage({"command": "getTV"}, (response) => {
+chrome.runtime.sendMessage({ "command": "getTV" }, (response) => {
 
 	document.querySelector("#tvshowRow").innerHTML = "";
-    for (const key in response.data) {
+	for (const key in response.data) {
+		let season = response.data[key];
+		for(const show in season){
+			var posterURL = ""
+			const baseImagePath = "https://image.tmdb.org/t/p/original/"
 
-		var posterURL = ""
-		const baseImagePath = "https://image.tmdb.org/t/p/original/"
-
-		fetch(`https://api.themoviedb.org/3/search/tv?api_key=c9e5f6b44a8037f35894aef02579205a&language=en-US&page=1&query=${key}&include_adult=false`)
-			.then(response => {
-				return response.json()
-			})
-			.then(data => {
-				var tvID = data.results[0].id
-				posterURL = baseImagePath + data.results[0].poster_path
-				var htmlToAdd = 
-				`<div class="col-md-6 col-lg-4 col-xl-3 pb-4">
-					<div class="card h-100">
-						<img src="${posterURL}" class="card-img-top" alt="...">
-						<div class="card-body">
-							<h5 class="card-title">${key}</h5>
-							<div class="card-footer">
-								<small class="text-muted">Rating: 4/5</small>
+			fetch(`https://api.themoviedb.org/3/search/tv?api_key=c9e5f6b44a8037f35894aef02579205a&language=en-US&page=1&query=${key}&include_adult=false`)
+				.then(response => {
+					return response.json()
+				})
+				.then(data => {
+					var tvID = data.results[0].id
+					posterURL = baseImagePath + data.results[0].poster_path
+					var htmlToAdd =
+						`
+						<div class="col-md-6 col-lg-4 col-xl-3 pb-4">
+						<div class="card h-100">					
+							<img src="${posterURL}" class="card-img-top" alt="..."/>
+							<div class="card-body">	
+								<h5 class="card-title">${key}</h5>
+								<div class="card-footer">
+									<small class="text-muted">Rating: 4/5</small>
+								</div>
+								<a class="stretched-link" role="button" data-toggle="modal" data-target="#myModal2"></a>
 							</div>
 						</div>
-					</div>
-				</div>`
-				document.querySelector("#tvshowRow").innerHTML += htmlToAdd;
-			})
-			.catch(error => {
-				console.log(error);
-			})
-    }
+						<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
+						<div class="modal-dialog modal-dialog-centered">
+    						<div class="modal-content">
+      							<div class="modal-header">
+        							<h5 class="modal-title" id="myModalLabel">Title</h5>
+        							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          								<span aria-hidden="true">&times;</span>
+        							</button>
+      							</div>
+      							<div class="modal-body">
+        							<p><a href="${season[show]}" target="_blank">Keep watching</a></p>
+      							</div>
+      							<div class="modal-footer">
+        							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        							<button type="button" class="btn btn-primary">Save changes</button>
+      							</div>
+    						</div>
+  						</div>
+						`
+					document.querySelector("#tvshowRow").innerHTML += htmlToAdd;
+				})
+				.catch(error => {
+					console.log(error);
+				})
+			}
+	}
 
 });
 
-/*	
-	Display a card with the title and episode. 
+/*
+	Display a card with the title and episode.
 	@param media type of media (Movie, Anime, Show).
-	@return none. 
+	@return none.
 */
 // function displayCard(media) {
 // 	for(title in media){
-		
+
 // 	}
 // }
 
